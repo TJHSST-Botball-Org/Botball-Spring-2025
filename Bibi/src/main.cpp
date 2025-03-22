4c65975150ad92a01e666bba65b1a943f19d67a6
Devin Park <devinpark.dp@gmail.com>
2025-03-22 15:48:29 -0400

    got hamburger and hotdog

--- Bibi/src/main.cpp
+++ Bibi/src/main.cpp
@@ -4,12 +4,40 @@
 int main()
 {
     Robot robot;
+
+    // beginning
+    robot.openClaw_Entree();
+    robot.move_linear(6.25, -46);
+
+    // grabs burger
+    robot.closeClaw_Entree();
+
+    // moves back to black tape
+    robot.move_linear(6.25, 25);
+    robot.turn_around_left_wheel(90.0);
+
+    // move across board and turns to drop off burger
+    robot.move_linear(6.25, -43);
+    robot.turn_around_left_wheel(90.0);
+
+    // moves back
+    robot.move_linear(6.25, -10);
+    robot.openClaw_Entree();
+    robot.move_linear(6.25, 10);
     robot.turn_around_left_wheel(90);
-    // robot.openClaw_Entree();
-    // robot.move_linear(5, -45);
-    // robot.closeClaw_Entree();
-    // robot.move_linear(5, 25);
-    // robot.turn_around_left_wheel(90.0);
+    robot.move_linear(6.25, -55);
+    robot.move_linear(6.25, 10);
+    robot.turn_around_left_wheel(90);
+    robot.move_linear(6.25, -17);
+
+    // grabs hotdog
+    robot.closeClaw_Entree();
+    robot.move_linear(6.25, 29);
+    robot.turn_around_left_wheel(90.0);
+    robot.move_linear(6.25, -53);
+    robot.turn_around_left_wheel(90.0);
+    robot.move_linear(6.25, -10);
+    robot.openClaw_Entree();
 
     // robot.move(-10000, 1000);
     // robot.openClaw_Entree();
